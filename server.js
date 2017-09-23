5084e5981dd46e1586943a8c410f9fbd035c8306
sanatankumar <sanatan.iit@gmail.com>
2017-09-23 18:12:53 +0530

    Basic app completed | Some Error Handling Left

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express')
 const { naturalToUnix, unixToNatural } = require('./helpers')
+const app = express()
+
+app.get('/:time', (req, res) => {
+    const time = req.params.time
+
+    if (+time) {
+        res.json({
+            unix: +time,
+            natural: unixToNatural(+time)
+        })
+    } else {
+        res.json({
+            unix: naturalToUnix(time),
+            natural: time
+        })
+    }
+})
+
+app.listen(4000)
+console.log('listening on port 4000 ...')
+
